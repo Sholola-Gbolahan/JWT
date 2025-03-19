c075788148ca0f10e14c34be932068936aee78d0
Gbolahan <shololagbolahan@gmail.com>
2025-03-19 09:33:24 +0100

    3. JWT sign setup done

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,7 @@
 // Check username, password in post(login) request
 // If exit create new JWT
 // Send back to frontend
+const jwt = require("jsonwebtoken")
 
 const CustomAPIError = require("../errors/custom-error")
 
@@ -15,13 +16,23 @@ const login = async (req, res) => {
     throw new CustomAPIError("please provide email and password", 400)
   }
 
-  res.send("Fake Login/Register/Signup")
+  // This is a demo, usually provided by DB!!!!
+  const id = new Date().getDate()
+
+  // using the sign func where we pass the payload and secrets
+  // Note: Try to keep payload small, better experience for user
+  // Note : Just for Demo,in production, use long, complex, unguessable string value!!!!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  })
+  res.status(200).json({ msg: "user created", token })
 }
+
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100) // generating some random numbers
   res.status(200).json({
     msg: "Hello, John Doe",
-    secret: `Here's your authorizated data, your lucky number is ${luckyNumber}`,
+    secret: `Here's your authorized data, your lucky number is ${luckyNumber}`,
   })
 }
 
