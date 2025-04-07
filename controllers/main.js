bfb81890b1cf08de04c0ee386dc66f38fbee0cc5
Gbolahan <shololagbolahan@gmail.com>
2025-04-07 05:49:55 +0100

    8. Error setup for Bad-request and Unauthenticated (Better Approach)

--- controllers/main.js
+++ controllers/main.js
@@ -30,6 +30,8 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
+  console.log(req.user)
+
   const luckyNumber = Math.floor(Math.random() * 100)
 
   res.status(200).json({
---
+++ errors/index.js
@@ -0,0 +1,5 @@
+const BadRequest = require("./bad-request")
+const CustomAPIError = require("./custom-error")
+const Unauthenticated = require("./unauthenticated")
+
+module.exports = { BadRequest, CustomAPIError, Unauthenticated }
